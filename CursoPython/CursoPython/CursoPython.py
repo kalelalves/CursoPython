ef3d16a0c0bb2dbb5106fe348446b826790619f3
Kalel Jamil Alves <kalel_fipp@hotmail.com>
2021-01-19 22:19:58 -0300

    TABELA  VERDADE

--- CursoPython/CursoPython/CursoPython.py
+++ CursoPython/CursoPython/CursoPython.py
@@ -11,3 +11,32 @@ print(9.4//3)
 print(2**8)
 print(10%3)
 
+#DESAFIO OPERADORES 
+salario= 3000.00
+despesas= 1500.00
+percentual_comprometido = despesas/ salario*100
+print("O percentual comprometido do salário é: "+str(percentual_comprometido)+'% ')
+
+#OPERADORES LÓGICOS
+
+True or False
+7 != 3 and 2 > 3
+
+#TABELA VERDADE DO AND
+True and True #Verdadeiro
+True and False #Falso
+False and True #Falso
+False and False #Falso
+
+#TABELA VERDADE DO AND
+True or True # Verdadeiro
+True or False#  Verdadeiro
+False or True# Verdadeiro
+False or False # Falso
+
+#TABELA VERDADE XOR
+True != True # Falso 
+True != False#  Verdadeiro
+False != True# Verdadeiro
+False != False # Falso
+
